1cab077e7c4a07133fb4ae26656c96b31966a737
Bhupender2004 <officialbhuppiiydv@gmail.com>
2026-05-25 22:02:37 +0530

    add implementation for checking Strong Numbers

--- training_day1.java
+++ training_day1.java
@@ -33,3 +33,31 @@
 //         System.out.print(sum);
 //     }
 // }
+
+
+//Strong Number..
+
+// import java.util.*;
+// class Main{
+//     public static void main(String [] args){
+//         Scanner sc  = new Scanner(System.in);
+//         System.out.print("Enter a no.: ");
+//         int n = sc.nextInt();
+//         int temp=n;
+//         int sum=0;
+//         while(n!=0){
+//             int dig = n%10;
+//             int fact=1;
+//             for(int i=1; i<=dig; i++){
+//                 fact=fact*i;
+//             }
+//             sum=sum+fact;
+//             n=n/10;
+//         }
+//         if(sum==temp){
+//             System.out.println("Strong Number");
+//         }else{
+//             System.out.println("Not a Strong Number");
+//         }
+//     }
+// }
