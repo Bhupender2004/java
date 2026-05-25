8371995ceecabb97a93269aac7e346c3ed391529
Bhupender2004 <officialbhuppiiydv@gmail.com>
2026-05-25 13:47:03 +0530

    sum of digit and duplicated checking in array

---
+++ training_day1.java
@@ -0,0 +1,35 @@
+// import java.util.Scanner;
+
+// public class training_day1 {
+//     public static void main(String [] args){
+//         Scanner sc = new Scanner(System.in);
+//         int [] arr1={1,2,3,4};
+//         int [] arr2={2,3,5,6};
+        
+//         for(int i=0; i<arr1.length; i++){
+//             for(int j=0; j<arr2.length; j++){
+//                 if(arr1[i]==arr2[j]){
+//                     System.out.print(arr1[i]+" ");
+//                 }
+//             }
+//         }
+//     }
+// }
+
+
+
+// sum of the digit of a given no.
+// import java.util.*;
+// class Main{
+//     public static void main(String [] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter a no.: ");
+//         int n = sc.nextInt();
+//         int sum=0;
+//         while(n!=0){
+//             sum+=n%10;
+//             n=n/10;
+//         }
+//         System.out.print(sum);
+//     }
+// }
