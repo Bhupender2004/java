9f430264c999cc173fdce1f8a1b4a43f5bab811c
Bhupender2004 <officialbhuppiiydv@gmail.com>
2026-05-26 10:21:07 +0530

    add implementation for finding the second largest element in an array without built-in functions

--- training_day2.java
+++ training_day2.java
@@ -20,3 +20,39 @@
 //         System.out.print(max);
 //     }
 // }
+
+
+// second max without using any built in function
+
+// import java.util.Scanner;
+
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         int n = sc.nextInt();
+
+//         int[] arr = new int[n];
+
+//         for (int i = 0; i < n; i++) {
+//             arr[i] = sc.nextInt();
+//         }
+
+//         int max = 0;
+//         int secondMax = 1;
+
+//         for (int i = 0; i < n; i++) {
+//             if (arr[i] > max) {
+//                 secondMax = max;
+//                 max = arr[i];
+//             }
+//             else if (arr[i] > secondMax && arr[i] != max) {
+//                 secondMax = arr[i];
+//             }
+//         }
+      
+//             System.out.println("Second Largest Element is " + secondMax);
+
+//         sc.close();
+//     }
+// }
