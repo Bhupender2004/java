3b20f0e09aa679b665f1042adf3977f6e6c2e9ca
Bhupender2004 <officialbhuppiiydv@gmail.com>
2026-05-28 22:46:45 +0530

    merge sort implementation

--- training_day3.java
+++ training_day3.java
@@ -1,63 +1,63 @@
-// Merge sort 
+// // Merge sort 
 
-import java.util.*;
-class Main{
-    static void mergeSort(int arr[], int left, int right){
-        if(left<right){
-            int mid=(left+right)/2;
-            mergeSort(arr, left, mid);
-            mergeSort(arr, mid+1, right);
-            merge(arr, left, mid, right);
-        }
-    }
+// import java.util.*;
+// class Main{
+//     static void mergeSort(int arr[], int left, int right){
+//         if(left<right){
+//             int mid=(left+right)/2;
+//             mergeSort(arr, left, mid);
+//             mergeSort(arr, mid+1, right);
+//             merge(arr, left, mid, right);
+//         }
+//     }
     
-    static void merge(int arr[], int left, int mid, int right){
-        int temp[]=new int[right-left+1];
-        int i=left;
-        int j=mid+1;
-        int k=0;
+//     static void merge(int arr[], int left, int mid, int right){
+//         int temp[]=new int[right-left+1];
+//         int i=left;
+//         int j=mid+1;
+//         int k=0;
         
-        while(i<=mid&&j<=right){
-            if(arr[i]<=arr[j]){
-                temp[k]=arr[i];
-                i++;
-            }else{
-                temp[k]=arr[j];
-                j++;
-            }
-            k++;
-        }
+//         while(i<=mid&&j<=right){
+//             if(arr[i]<=arr[j]){
+//                 temp[k]=arr[i];
+//                 i++;
+//             }else{
+//                 temp[k]=arr[j];
+//                 j++;
+//             }
+//             k++;
+//         }
         
-        while(i<=mid){
-            temp[k]=arr[i];
-            i++;
-            k++;
-        }
+//         while(i<=mid){
+//             temp[k]=arr[i];
+//             i++;
+//             k++;
+//         }
         
-        while(j<=right){
-            temp[k]=arr[j];
-            j++;
-            k++;
-        }
+//         while(j<=right){
+//             temp[k]=arr[j];
+//             j++;
+//             k++;
+//         }
         
-        for(i=left, k=0; i<=right; i++, k++){
-            arr[i]=temp[k];
-        }
-    }
+//         for(i=left, k=0; i<=right; i++, k++){
+//             arr[i]=temp[k];
+//         }
+//     }
     
-    public static void main(String [] args){
-        Scanner sc = new Scanner(System.in);
-        int n=sc.nextInt();
-        int arr[]=new int[n];
+//     public static void main(String [] args){
+//         Scanner sc = new Scanner(System.in);
+//         int n=sc.nextInt();
+//         int arr[]=new int[n];
         
-        for(int i=0; i<n; i++){
-            arr[i]=sc.nextInt();
-        }
+//         for(int i=0; i<n; i++){
+//             arr[i]=sc.nextInt();
+//         }
         
-        mergeSort(arr,0,n-1);
+//         mergeSort(arr,0,n-1);
         
-        for(int i=0; i<n; i++){
-            System.out.print(arr[i]+" ");
-        }
-    }
-}
+//         for(int i=0; i<n; i++){
+//             System.out.print(arr[i]+" ");
+//         }
+//     }
+// }
