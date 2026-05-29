30d9db55d88a37de58c6ccc7e4154911f4121d26
Bhupender2004 <officialbhuppiiydv@gmail.com>
2026-05-29 23:45:51 +0530

    refactor: remove merge sort implementation and add twoSum method

--- training_day3.java
+++ training_day3.java
@@ -61,3 +61,19 @@
 //         }
 //     }
 // }
+
+
+// Given an array of integers numbers that is sorted in non-decreasing order.
+// public class training_day3 {
+//     public int[] twoSum(int[] numbers, int target) {
+//         for (int i = 0; i < numbers.length; i++) {
+//             for (int j = i + 1; j < numbers.length; j++) {
+//                 if (numbers[i] + numbers[j] == target) {
+//                     return new int[] { i + 1, j + 1 };
+//                 }
+//             }
+//         }
+//         return new int[0];
+//     }
+// }
+
