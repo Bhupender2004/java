532ce547bef2e9131ead78e8f4803ff64f086eff
Bhupender2004 <officialbhuppiiydv@gmail.com>
2026-06-03 11:40:45 +0530

    add implementation to find missing number in an array using bit manipulation

--- training_day7.java
+++ training_day7.java
@@ -40,3 +40,25 @@
 
 //     }
 // }
+
+
+// Find missing no. in the array using bit opertion.
+import java.util.*;
+class training_day7{
+    public static void main(String [] args){
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
+        int val=0;
+        for(int i=0; i<=n; i++){
+            val=val^i;
+        }
+        for(int i=0; i<n; i++){
+            val=val^arr[i];
+        }
+        System.out.print(val);
+    }
+}
