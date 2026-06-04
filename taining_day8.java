b0d2d115ecf21d703a99d941af7a216364c0fa19
Bhupender2004 <officialbhuppiiydv@gmail.com>
2026-06-04 12:31:21 +0530

    Fibonacci series implementation to use recursion

--- taining_day8.java
+++ taining_day8.java
@@ -1,16 +1,40 @@
+// import java.util.*;
+// class training_day8{
+//     public static void printNum(int n){
+//         if(n==0){
+//             return;
+//         }
+//         printNum(n-1);
+//         System.out.print(n+" ");
+//     }
+//     public static void main(String [] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter the no.: ");
+//         int n=sc.nextInt();
+//         printNum(n);
+//     }
+// }
+
+
+// Fibonacci series using recursion.
+
 import java.util.*;
 class training_day8{
-    public static void printNum(int n){
+    public static int printFib(int a, int b, int n){
         if(n==0){
-            return;
+            return 1;
         }
-        printNum(n-1);
-        System.out.print(n+" ");
+        int c=a+b;
+        System.out.print(c+" ");
+        return printFib(b,c,n-1);
     }
     public static void main(String [] args){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the no.: ");
+        System.out.print("Enter no. : ");
         int n=sc.nextInt();
-        printNum(n);
+        int a=0, b=1;
+        System.out.print(a+" ");
+        System.out.print(b+" ");
+        printFib(a,b,n-2);
     }
 }
