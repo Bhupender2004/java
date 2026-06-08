f605e14a40e1bb6d1a5fda85c4dc3d217a1fb409
Bhupender2004 <officialbhuppiiydv@gmail.com>
2026-06-08 13:31:24 +0530

    method for CGPA calculation

--- training_day10.java
+++ training_day10.java
@@ -1,22 +1,38 @@
-import java.util.*;
-public class training_day10 {
-    public static void subset(int arr[], int idx, String curr ){
-        if(idx==arr.length){
-            System.out.println("{"+curr+"}");
-            return;
-        }
-        subset(arr, idx+1, curr);
-        subset(arr, idx+1, curr+arr[idx]+" ");
-    }
+// import java.util.*;
+// public class training_day10 {
+//     public static void subset(int arr[], int idx, String curr ){
+//         if(idx==arr.length){
+//             System.out.println("{"+curr+"}");
+//             return;
+//         }
+//         subset(arr, idx+1, curr);
+//         subset(arr, idx+1, curr+arr[idx]+" ");
+//     }
 
-    public static void main(String [] ar){
+//     public static void main(String [] ar){
+//         Scanner sc = new Scanner(System.in);
+//         int n=sc.nextInt();
+//         int arr[]=new int[n];
+//         for(int i=0; i<n; i++){
+//             arr[i]=sc.nextInt();
+//         }
+//         subset(arr,0," ");
+//     }
+// }
+
+
+import java.util.Scanner;
+class training_day10{
+    public static void main(String [] arg){
         Scanner sc = new Scanner(System.in);
-        int n=sc.nextInt();
-        int arr[]=new int[n];
-        for(int i=0; i<n; i++){
-            arr[i]=sc.nextInt();
-        }
-        subset(arr,0," ");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        float total=(float)(a+b+c);
+        // float tot=300.0;
+        float ans=total/300;
+        float cgpa=ans*10;
+        
+        System.out.printf("%.2f", cgpa);
     }
 }
-
