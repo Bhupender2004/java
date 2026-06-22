f45a41f324e231ad686f7b9826d7df3360091f43
Bhupender2004 <officialbhuppiiydv@gmail.com>
2026-06-22 13:56:56 +0530

    Automorphic number

--- cst.java
+++ cst.java
@@ -1,3 +1,26 @@
-public class cst {
-    
+// Automorphic Number 
+
+import java.util.Scanner;
+class cst {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if (isAutomorphic(num)) {
+            System.out.println("Automorphic");
+        } else {
+            System.out.println("Not Automorphic");
+        }
+    }
+    public static boolean isAutomorphic(int n) {
+        int sq = n * n; 
+        while (n > 0) {
+            if (n % 10 != sq % 10) {
+                return false; 
+            }
+            n /= 10;      
+            sq /= 10; 
+        }
+        return true; 
+    }
 }
+
