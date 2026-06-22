36dca5b20addc7e7d59bea67d6abe5a10983fce0
Bhupender2004 <officialbhuppiiydv@gmail.com>
2026-06-22 14:28:42 +0530

    gcd program

--- cst.java
+++ cst.java
@@ -38,3 +38,21 @@
 //         System.out.println(count);
 //     }
 // }
+
+
+// gcd
+// import java.util.*;
+// public class cst {
+//     public static int gcd(int a, int b){
+//         if(b==0){
+//             return a;
+//         }
+//         return gcd(b, a%b);
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a=sc.nextInt();
+//         int b=sc.nextInt();
+//         System.out.print(gcd(a,b));
+//     }
+// }
