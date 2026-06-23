920b04ea4b3df5fb3ae272a54def8f9ce85247f5
Bhupender2004 <officialbhuppiiydv@gmail.com>
2026-06-23 21:22:56 +0530

    gun&baloon program fix

--- cst.java
+++ cst.java
@@ -63,29 +63,29 @@
 
 // Gun and Baloon Problem
 
-import java.util.*;
-public class cst {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        if (!sc.hasNextInt()) return;
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-        int[] b = new int[n];
-        for (int i = 0; i < n; i++) {
-            b[i] = sc.nextInt();
-        }
-        int minutes = 0;
-        while (true) {
-            Arrays.sort(b);
-            if (b[n - k] > 0) {
-                minutes++;
-                for (int i = n - 1; i >= n - k; i--) {
-                    b[i]--;
-                }
-            } else {
-                break; 
-            }
-        }
-        System.out.println(minutes);
-    }
-}
+// import java.util.*;
+// public class cst {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         if (!sc.hasNextInt()) return;
+//         int n = sc.nextInt();
+//         int k = sc.nextInt();
+//         int[] b = new int[n];
+//         for (int i = 0; i < n; i++) {
+//             b[i] = sc.nextInt();
+//         }
+//         int minutes = 0;
+//         while (true) {
+//             Arrays.sort(b);
+//             if (b[n - k] > 0) {
+//                 minutes++;
+//                 for (int i = n - 1; i >= n - k; i--) {
+//                     b[i]--;
+//                 }
+//             } else {
+//                 break; 
+//             }
+//         }
+//         System.out.println(minutes);
+//     }
+// }
