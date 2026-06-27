cc8aa5cc1c1354cc1fc4cc641db4dfc63524e504
Bhupender2004 <officialbhuppiiydv@gmail.com>
2026-06-27 22:19:26 +0530

    Pizza Party problem using sliding window technique

--- cst.java
+++ cst.java
@@ -203,3 +203,38 @@
 //         System.out.println(totalEvenSum);
 //     }
 // }
+
+
+
+// Pizza Party Problem
+
+// import java.util.*;
+// public class cst {
+//     public static void main(String args[] ) throws Exception {
+//         /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+//         Scanner sc = new Scanner(System.in);
+//         int n=sc.nextInt();
+//         int k=sc.nextInt();
+//         int arr[]=new int[n];
+//         for(int i=0; i<n; i++){
+//             arr[i]=sc.nextInt();
+//         }    
+//         int limit=k-1;
+//         HashMap<Integer, Integer> map=new HashMap<>();
+//         int left=0;
+//         int maxLen=0;
+//         for(int right=0; right<n; right++){
+//             map.put(arr[right], map.getOrDefault(arr[right],0)+1);
+//             while(map.size()>limit){
+//                 map.put(arr[left], map.get(arr[left])-1);
+                
+//                 if(map.get(arr[left])==0){
+//                     map.remove(arr[left]);
+//                 }
+//                 left++;
+//             }
+//             maxLen=Math.max(maxLen, right-left+1);
+//         }
+//         System.out.println(maxLen);
+//     }
+// }
