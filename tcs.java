c392e69fd53a64cdc0de0dfcec0b5e14a8af6d3f
Bhupender2004 <officialbhuppiiydv@gmail.com>
2026-07-09 18:17:28 +0530

    rectangle overlap functionality

--- tcs.java
+++ tcs.java
@@ -66,22 +66,55 @@
 
 // prime no.
 
-import java.util.*;
+// import java.util.*;
+// class tcs{
+//     public static void main(String [] args){
+//         Scanner sc = new Scanner(System.in);
+//         int n=sc.nextInt();
+//         for(int i=2; i<=n; i++){
+//             boolean isPrime=true;
+//             for(int j=2; j<i; j++){
+//                 if(i%j==0){
+//                     isPrime=false;
+//                     break;
+//                 }
+//             }
+//             if(isPrime){
+//                 System.out.print(i+" ");
+//             }
+//         }
+//     }
+// }
+
+
+// Rectangle Overlap
 class tcs{
+    static class Point{
+        int x,y;
+        Point(int x, int y){
+            this.x=x;
+            this.y=y;
+        }
+    }
+    static boolean doOverlap(Point l1, Point r1, Point l2, Point r2){
+        if(l1.x>r2.x||l2.x>r1.x){
+            return false;
+        }
+        if(r1.y>l2.y||r2.y>l1.y){
+            return false;
+        }
+        return true;
+    }
     public static void main(String [] args){
-        Scanner sc = new Scanner(System.in);
-        int n=sc.nextInt();
-        for(int i=2; i<=n; i++){
-            boolean isPrime=true;
-            for(int j=2; j<i; j++){
-                if(i%j==0){
-                    isPrime=false;
-                    break;
-                }
-            }
-            if(isPrime){
-                System.out.print(i+" ");
-            }
+        Point l1=new Point(0, 10);
+        Point r1=new Point(10, 0);
+        Point l2=new Point(5, 5);
+        Point r2=new Point(15, 0);
+        
+        if(doOverlap(l1, r1, l2, r2)){
+            System.out.println("Rectangle overlap.");
+        }else{
+            System.out.print("Rectangle don't overlap.");
         }
     }
 }
