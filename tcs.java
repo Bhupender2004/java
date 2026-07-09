f45da94e6ce1b16c16c0f575e68264d8375f4948
Bhupender2004 <officialbhuppiiydv@gmail.com>
2026-07-09 13:23:50 +0530

    prime number

--- tcs.java
+++ tcs.java
@@ -29,36 +29,59 @@
 
 // Second Largest
 
+// import java.util.*;
+// public class tcs {
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int n=sc.nextInt();
+//         int arr[]=new int[n];
+//         for(int i=0; i<n; i++){
+//             arr[i]=sc.nextInt();
+//         }
+//         Arrays.sort(arr);
+//         for(int i=0; i<n; i++){
+//             System.out.print(arr[i]+" ");
+//         }
+//         System.out.println();
+//         int largest=Integer.MIN_VALUE;
+//         int secondLargest=Integer.MIN_VALUE;
+
+//         for(int i=0; i<n; i++){
+//             if(arr[i]>largest){
+//                 secondLargest=largest;
+//                 largest=arr[i];
+//             }else if(arr[i]>secondLargest&&arr[i]!=largest){
+//                 secondLargest=largest;
+//             }
+//         }
+
+//         if(secondLargest==Integer.MIN_VALUE){
+//             System.out.print("Second Largest doesn't exist.");
+//         }else{
+//             System.out.print(secondLargest);
+//         }
+//     }
+// }
+
+
+// prime no.
+
 import java.util.*;
-public class tcs {
-    public static void main(String[] args){
+class tcs{
+    public static void main(String [] args){
         Scanner sc = new Scanner(System.in);
         int n=sc.nextInt();
-        int arr[]=new int[n];
-        for(int i=0; i<n; i++){
-            arr[i]=sc.nextInt();
-        }
-        Arrays.sort(arr);
-        for(int i=0; i<n; i++){
-            System.out.print(arr[i]+" ");
-        }
-        System.out.println();
-        int largest=Integer.MIN_VALUE;
-        int secondLargest=Integer.MIN_VALUE;
-
-        for(int i=0; i<n; i++){
-            if(arr[i]>largest){
-                secondLargest=largest;
-                largest=arr[i];
-            }else if(arr[i]>secondLargest&&arr[i]!=largest){
-                secondLargest=largest;
+        for(int i=2; i<=n; i++){
+            boolean isPrime=true;
+            for(int j=2; j<i; j++){
+                if(i%j==0){
+                    isPrime=false;
+                    break;
+                }
             }
-        }
-
-        if(secondLargest==Integer.MIN_VALUE){
-            System.out.print("Second Largest doesn't exist.");
-        }else{
-            System.out.print(secondLargest);
+            if(isPrime){
+                System.out.print(i+" ");
+            }
         }
     }
 }
