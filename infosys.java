3e1324f1420f83d27e195e7a2710177852bbafd7
Bhupender2004 <officialbhuppiiydv@gmail.com>
2026-07-10 22:34:47 +0530

    containsDuplicate method to check for duplicate integers in an array

---
+++ infosys.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class infosys {
+    public boolean containsDuplicate(int [] nums){
+        HashSet<Integer> set=new HashSet<>();
+        for(int num:nums){
+            if(set.contains(num)){
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+}
