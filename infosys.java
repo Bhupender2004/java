aabdf648b07ea7279c5ba880620521a6c84e08c6
Bhupender2004 <officialbhuppiiydv@gmail.com>
2026-07-11 17:24:53 +0530

    convert seconds into hours, minutes, and seconds

--- infosys.java
+++ infosys.java
@@ -18,26 +18,47 @@
 // 2. Valid Anagram
 
 
+// import java.util.*;
+// class infosys{
+//     public static boolean isAnagram(String s, String t){
+//         if(s.length()!=t.length()){
+//             return false;
+//         }
+//         char [] sSort=s.toCharArray();
+//         char [] tSort=t.toCharArray();
+//         Arrays.sort(sSort);
+//         Arrays.sort(tSort);
+//         if(Arrays.equals(sSort, tSort)){
+//             return true;
+//         }
+//         return false;
+//     }
+//     public static void main(String [] args){
+//         Scanner sc= new Scanner(System.in);
+//         String s=sc.nextLine();
+//         String t=sc.nextLine();
+//         System.out.print(isAnagram(s,t));
+//     }
+// }
+
+
+
+// Program to convert seconds into hour, min and second.
 import java.util.*;
 class infosys{
-    public static boolean isAnagram(String s, String t){
-        if(s.length()!=t.length()){
-            return false;
-        }
-        char [] sSort=s.toCharArray();
-        char [] tSort=t.toCharArray();
-        Arrays.sort(sSort);
-        Arrays.sort(tSort);
-        if(Arrays.equals(sSort, tSort)){
-            return true;
-        }
-        return false;
-    }
     public static void main(String [] args){
-        Scanner sc= new Scanner(System.in);
-        String s=sc.nextLine();
-        String t=sc.nextLine();
-        System.out.print(isAnagram(s,t));
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter seconds: ");
+        int seconds=sc.nextInt();
+
+        int hr=seconds/3600;
+        int remSec=seconds%3600;
+
+        int min=remSec/60;
+        int sec=remSec%60;
+
+        System.out.println("Hour: "+hr);
+        System.out.println("Min: "+min);
+        System.out.println("Sec: "+sec);
     }
 }
-
