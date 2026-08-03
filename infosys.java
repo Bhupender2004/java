5a54dd1b182911b9f636174a789ba4e1a13c5aff
Bhupender2004 <officialbhuppiiydv@gmail.com>
2026-08-03 09:01:45 +0530

    Fibonacci series using recursion

--- infosys.java
+++ infosys.java
@@ -434,18 +434,41 @@
 
 
 // Fibonacci Series
-import java.util.*;
-class infosys{
-    public static void main(String [] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter n: ");
-        int n=sc.nextInt();
-        int a=0; int b=1;
-        for(int i=1; i<=n; i++){
-            System.out.print(a+" ");
-            int next=a+b;
-            a=b;
-            b=next;
-        }
-    }
-}
+// import java.util.*;
+// class infosys{
+//     public static void main(String [] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter n: ");
+//         int n=sc.nextInt();
+//         int a=0; int b=1;
+//         for(int i=1; i<=n; i++){
+//             System.out.print(a+" ");
+//             int next=a+b;
+//             a=b;
+//             b=next;
+//         }
+//     }
+// }
+
+
+// Fibonacci series using recursion.
+// import java.util.*;
+// class infosys{
+//     public static void printFib(int a, int b, int n){
+//         if(n==0){
+//             return;
+//         }
+//         int c=a+b;
+//         System.out.print(c+" ");
+//         printFib(b,c,n-1);
+//     }
+//     public static void main(String [] args){
+//         Scanner sc=new Scanner(System.in);
+//         System.out.print("Enter n: ");
+//         int n=sc.nextInt();
+//         int a=0; int b=1;
+//         System.out.print(a+" ");
+//         System.out.print(b+" ");
+//         printFib(a,b,n-2);
+//     }
+// }
