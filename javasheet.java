925b0e05b5089878852691bff6a54b45a6b3a870
Bhupender2004 <officialbhuppiydv@gmail.com>
2026-09-14 23:56:42 +0530

    toggle case program

--- javasheet.java
+++ javasheet.java
@@ -238,17 +238,42 @@
 
 
 // Pprogram to replace all occurrences of a character with another character in a string.
+// import java.util.*;
+// class javasheet{
+//     public static void main(String [] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter string: ");
+//         String str=sc.nextLine();
+//         System.out.print("Enter character to replace: ");
+//         char oldchar=sc.next().charAt(0);
+//         System.out.print("Enter new character: ");
+//         char newchar=sc.next().charAt(0);
+//         String result=str.replace(oldchar, newchar);
+//         System.out.print("Updated string: "+result);
+//     }
+// }
+
+
+// Program to toggle to case of each character in a string.
 import java.util.*;
-class javasheet{
-    public static void main(String [] args){
+class javasheet {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter string: ");
-        String str=sc.nextLine();
-        System.out.print("Enter character to replace: ");
-        char oldchar=sc.next().charAt(0);
-        System.out.print("Enter new character: ");
-        char newchar=sc.next().charAt(0);
-        String result=str.replace(oldchar, newchar);
-        System.out.print("Updated string: "+result);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch >= 'A' && ch <= 'Z') {
+                result += (char)(ch + 32);
+            }
+            else if (ch >= 'a' && ch <= 'z') {
+                result += (char)(ch - 32);
+            }
+            else {
+                result += ch;
+            }
+        }
+        System.out.println("Toggled string: " + result);
     }
 }
